6cd75cec538d17bc8378d9ebe6a26dbff4a84c75
Casey Cannon <36081765+caseycannon423@users.noreply.github.com>
2019-01-17 14:08:05 -0800

    Uploaded original script

---
+++ StarChart.py
@@ -0,0 +1,43 @@
+T = [[9, 3], [4, 4], [12, 2], [8, 3], [1, 2], [2, 1], [13, 3], [7, 5], [6, 2], [3, 2]]
+
+def findStart(Array):
+    iMax = 0
+    cell = Array[0]
+    for x in Array:
+        if x[1] > iMax:
+            iMax = x[1]
+            cell = x
+
+    return cell
+
+def findMaxChartDay(Array):
+    iLargestIndex = 0
+    for x in Array:
+        if x[0] > iLargestIndex:
+            iLargestIndex = x[0]
+    return iLargestIndex
+
+def printLine(starList, days):
+    for x in range(days):
+
+        for star in starList:
+            if x == star:
+                print('*', end='')
+            else:
+                print(' ', end='')
+    print()
+
+
+def main():
+    start = findStart(T)
+    days = findMaxChartDay(T)
+    starList = [start[1]]
+    for x in range(start[1]-1, 0, -1):
+        printLine(starList, days)
+        for cell in T:
+            if cell[1] == x:
+                starList.append(cell[0])
+
+
+main()
+
